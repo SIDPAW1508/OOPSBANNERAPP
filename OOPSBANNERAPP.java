ec55a51b9255cde8cd334c3bee45c7f37ee51013
SIDPAWW <siddhipawar.1508@gmail.com>
2026-02-20 12:30:00 +0530

    Printing oops using arrays

--- OOPSBANNERAPP.java
+++ OOPSBANNERAPP.java
@@ -2,17 +2,21 @@
 * OOPSBannerApp UC1-OOPS Banner Display Application 
 
 * @author Developer
-@version 1.0
+@version 3J.0
 */
 
-class Message{
+class UC4{
 	public static void main (String[] args){
-		System.out.println(String.join(" ","   ***  "," " ,"   **  ","  ", "****   " ,"  ","  ****  "));
-		System.out.println(String.join(" "," **   **"," " ," **  **","  ", "**  ** " ,"  "," **     "));
-		System.out.println(String.join(" "," **   **"," " ," **  **","  ", "**  ** " ,"  ","  ****  "));
-		System.out.println(String.join(" "," **   **"," " ," **  **","  ", "** *   " ,"  ","     ** "));
-		System.out.println(String.join(" "," **   **"," " ," **  **","  ", "**     " ,"  "," **   **")); 
-		System.out.println(String.join(" ","   ***  "," " ,"   **  ","  ", "**     " ,"  ","  ****  "));
+		String lines[]=new String[6];
+		lines[0]=String.join(" ","   ***  "," " ,"   **  ","  ", "****   " ,"  ","  ****  ");
+		lines[1]=String.join(" "," **   **"," " ," **  **","  ", "**  ** " ,"  "," **     ");
+		lines[2]=String.join(" "," **   **"," " ," **  **","  ", "**  ** " ,"  ","  ****  ");
+		lines[3]=String.join(" "," **   **"," " ," **  **","  ", "** *   " ,"  ","     ** ");
+		lines[4]=String.join(" "," **   **"," " ," **  **","  ", "**     " ,"  "," **   **"); 
+		lines[5]=String.join(" ","   ***  "," " ,"   **  ","  ", "**     " ,"  ","  ****  ");
+		for (String line:lines){
+			System.out.println(line);
+		}
 		
 	}
 	
