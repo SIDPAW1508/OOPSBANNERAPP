816011a8c9cf6075ae902d049dd21f68c66999df
SIDPAWW <siddhipawar.1508@gmail.com>
2026-02-20 12:26:36 +0530

    printing pattern using string.join

--- OOPSBANNERAPP.java
+++ OOPSBANNERAPP.java
@@ -7,12 +7,12 @@
 
 class Message{
 	public static void main (String[] args){
-		System.out.println("   ***  "+" " +"   **  "+"  "+ "****   " +"  "+"  ****  ");
-		System.out.println(" **   **"+" " +" **  **"+"  "+ "**  ** " +"  "+" **     ");
-		System.out.println(" **   **"+" " +" **  **"+"  "+ "**  ** " +"  "+"  ****  ");
-		System.out.println(" **   **"+" " +" **  **"+"  "+ "** *   " +"  "+"     ** ");
-		System.out.println(" **   **"+" " +" **  **"+"  "+ "**     " +"  "+" **   **"); 
-		System.out.println("   ***  "+" " +"   **  "+"  "+ "**     " +"  "+"  **** ");
+		System.out.println(String.join(" ","   ***  "," " ,"   **  ","  ", "****   " ,"  ","  ****  "));
+		System.out.println(String.join(" "," **   **"," " ," **  **","  ", "**  ** " ,"  "," **     "));
+		System.out.println(String.join(" "," **   **"," " ," **  **","  ", "**  ** " ,"  ","  ****  "));
+		System.out.println(String.join(" "," **   **"," " ," **  **","  ", "** *   " ,"  ","     ** "));
+		System.out.println(String.join(" "," **   **"," " ," **  **","  ", "**     " ,"  "," **   **")); 
+		System.out.println(String.join(" ","   ***  "," " ,"   **  ","  ", "**     " ,"  ","  ****  "));
 		
 	}
 	
